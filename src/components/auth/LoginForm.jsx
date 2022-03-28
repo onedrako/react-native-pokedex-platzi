b18da42837816c2173308b5ae4bd5408d7bb08cd
onedrako <onedrako@gmail.com>
2022-03-28 15:42:00 -0600

    Course Formularios y almacenamiento, Class 3 Working

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,12 +1,40 @@
-import { View, Text } from 'react-native'
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Button,
+  Keyboard
+} from 'react-native'
 import React from 'react'
 
 const LoginForm = () => {
   return (
     <View>
-      <Text>LoginForm</Text>
+      <Text style={styles.title}>Iniciar sesión</Text>
+      <TextInput placeholder='Nombre de Usuario' style={styles.input} autoCapitalize="none" />
+      <TextInput placeholder='Contraseña' style={styles.input} autoCapitalize="none" secureTextEntry={true} />
+      <Button title='Iniciar Sesión' onPress={() => Keyboard.dismiss()} />
+
     </View>
   )
 }
 
-export default LoginForm
+const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginTop: 50,
+    marginBottom: 15
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10
+  }
+})
+
+export { LoginForm }
