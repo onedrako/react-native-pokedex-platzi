264309abde29a870e24355789ff58087a6e12b68
onedrako <onedrako@gmail.com>
2022-03-28 13:47:40 -0600

    Class 17 working, end of course

--- src/screens/Pokemon.jsx
+++ src/screens/Pokemon.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { ScrollView } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome5'
+
 import { getPokemonDetailsApi } from '../api/pokemon'
 import { Header } from '../components/pokemon/Header'
 import { Type } from '../components/pokemon/Type'
@@ -8,6 +10,13 @@ import { Stats } from '../components/pokemon/Stats'
 const PokemonScreen = (props) => {
   const { route: { params }, navigation } = props
   const [pokemon, setPokemon] = useState(null)
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => null,
+      headerLeft: () => (<Icon name="arrow-left" color="#fff" size={20} style={{ marginLeft: 20 }} onPress={navigation.goBack}/>)
+    })
+  }, [navigation, params])
 
   useEffect(() => {
     (async () => {
