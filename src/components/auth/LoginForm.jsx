793691df060bf905bbeb060edd7a5a8b209fd57f
onedrako <onedrako@gmail.com>
2022-03-28 16:41:43 -0600

    Course Formularios y almacenamiento, Class 4 Working, using formik and yup

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -7,17 +7,63 @@ import {
   Keyboard
 } from 'react-native'
 import React from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 const LoginForm = () => {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formValues) => {
+      console.log(formValues)
+    }
+  })
+
   return (
     <View>
       <Text style={styles.title}>Iniciar sesión</Text>
-      <TextInput placeholder='Nombre de Usuario' style={styles.input} autoCapitalize="none" />
-      <TextInput placeholder='Contraseña' style={styles.input} autoCapitalize="none" secureTextEntry={true} />
-      <Button title='Iniciar Sesión' onPress={() => Keyboard.dismiss()} />
+      <TextInput
+        placeholder='Nombre de Usuario'
+        style={styles.input}
+        autoCapitalize="none"
+        // value={formik.values.username}
+        onChangeText = {(text) =>
+          formik.setFieldValue('username', text)
+        }
+        />
+      <Text style={styles.errors} >{formik.errors.username}</Text>
+      <TextInput
+        placeholder='Contraseña'
+        style={styles.input}
+        autoCapitalize="none"
+        secureTextEntry={true}
+        // value={formik.values.password}
+        onChangeText = {(text) =>
+          formik.setFieldValue('password', text)
+        }
+        />
+      <Text style={styles.errors} >{formik.errors.password}</Text>
+      <Button
+        title='Iniciar Sesión'
+        onPress={formik.handleSubmit}
+        />
 
     </View>
   )
+}
+
+const initialValues = () => {
+  return {
+    username: '',
+    password: ''
+  }
+}
+
+const validationSchema = () => {
+  return {
+    username: Yup.string().required('El nombre de usuario es requerido'),
+    password: Yup.string().required('La contraseña es requerida')
+  }
 }
 
 const styles = StyleSheet.create({
@@ -34,6 +80,10 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     borderRadius: 10
+  },
+  errors: {
+    textAlign: 'center',
+    color: 'red'
   }
 })
 
