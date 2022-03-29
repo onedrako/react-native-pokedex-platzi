35dc5001990df095548b90903e8798410c0d419b
onedrako <onedrako@gmail.com>
2022-03-29 10:57:03 -0600

    Course Formularios y almacenamiento, Class 8 Working, button add favs

--- src/screens/Pokemon.jsx
+++ src/screens/Pokemon.jsx
@@ -6,17 +6,21 @@ import { getPokemonDetailsApi } from '../api/pokemon'
 import { Header } from '../components/pokemon/Header'
 import { Type } from '../components/pokemon/Type'
 import { Stats } from '../components/pokemon/Stats'
+import { Favorite } from '../components/pokemon/Favorite'
+import useAuth from '../hooks/useAuth'
 
 const PokemonScreen = (props) => {
   const { route: { params }, navigation } = props
   const [pokemon, setPokemon] = useState(null)
 
+  const { auth } = useAuth()
+
   useEffect(() => {
     navigation.setOptions({
-      headerRight: () => null,
+      headerRight: auth ? () => (<Favorite id={pokemon?.id}/>) : null,
       headerLeft: () => (<Icon name="arrow-left" color="#fff" size={20} style={{ marginLeft: 20 }} onPress={navigation.goBack}/>)
     })
-  }, [navigation, params])
+  }, [navigation, params, pokemon])
 
   useEffect(() => {
     (async () => {
